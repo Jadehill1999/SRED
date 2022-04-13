00078ede783add89f3a53547675d24c275644ff1
Jadehill1999 <73286884+Jadehill1999@users.noreply.github.com>
2022-04-13 20:50:29 +0100

    properties and constructors added

---
+++ src/endUser/TestCase.java
@@ -0,0 +1,14 @@
+package endUser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,3 @@
 module SRED {
+	requires org.junit.jupiter.api;
 }
