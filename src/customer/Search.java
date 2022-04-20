df43d108c0e96e6a47c3654d858d017d850cf7db
Kathleen Edie <kathleen.edie@arnoldclark.com>
2022-04-20 22:37:22 +0100

    start on sort

--- src/customer/Search.java
+++ src/customer/Search.java
@@ -2,6 +2,7 @@ package customer;
 
 import java.time.*;
 import java.time.format.TextStyle;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import User.UserPreferences;
@@ -121,6 +122,18 @@ public class Search {
 	
 	// Location requirements
 	// Have a postcode letter list - assign a value and bubble sort?
+	public void searchResultsSort(List<EateryInformation> searchResults) {
+		
+		List<String> postCodeOrderRef = new ArrayList<String>(){
+			{
+				add("AB");
+				add("EH"); 
+				add("DD");
+				add("IV");
+			}
+		};
+		
+	}
 	// for this project use postcode numbers as ranks of closeness.
 	// apply list of matching customers (from above), iterate through and sort according to the postcode closeness.
 	// save and deliver the new list with customers in the correct order for the end user.  Must include cuisine information and time category
